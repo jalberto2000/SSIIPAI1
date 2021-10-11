f525dd7ece5eec94a403fb93ee1aeabe54737cdc
Antonio <antoniolofu99@gmail.com>
2021-10-11 11:49:24 +0200

    creacion de los logs

--- src/Users.py
+++ src/Users.py
@@ -1,17 +1,44 @@
 import sys, os, json
 from TreeInit import *
+from datetime import datetime
 
-def creaDirectorio(id):
+
+def creaUsuario(id):
     try:
         os.mkdir("./Users/" + str(id))
     except OSError:
-        print ("No se pudo crear la carpeta")
+        print ("No se pudo crear el usuario")
     else:
-        print ("Carpeta creada correctamente")
+        print ("Usuario creado")
 
 
-def inicializaArbolUsuario(id):
-    creaDirectorio(id)
+
+def creaLog(id):
+    fecha = datetime.now().strftime("%d/%m/%Y")
+    mes = datetime.now().strftime("%m %Y")
+    hora = datetime.now().strftime("%H:%M:%S")
+    direccionUsuario = "./Users/" + str(id)
+    f = open(direccionUsuario + "/" + mes +".txt","a+")
+    f.write("Analisis realizado el dia " + fecha + " a las " + hora + "\n")
+    f.close()
 
 
-creaDirectorio(12)
+def insertaLog(status, id, direccion):
+    fecha = datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
+    mes = datetime.now().strftime("%m %Y")
+    direccionUsuario = "./Users/" + str(id) 
+    if status:
+        f = open(direccionUsuario + "/" + mes +".txt","a+")
+        f.write("El archivo: " + direccion + " esta integro \n")
+        f.close()
+    else:
+        f = open(direccionUsuario + "/" + mes +".txt","a+")
+        f.write("El archivo: " + direccion + " ha sido modificado \n")
+        f.close()
+
+
+
+
+
+
+
